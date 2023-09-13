64219cbba28b8e3b428a385fc2d68d6cafaae7d3
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-09-13 14:31:49 +0530

    Add game() to script.js

--- script.js
+++ script.js
@@ -53,3 +53,35 @@ function playRound(playerSelection, computerSelection) {
   }
 
 }
+
+function game() {
+  let playRoundReturn;
+  let tally = 0;
+  
+  for(i = 0; i < 5; i++)
+  {
+    playRoundReturn = playRound(prompt("Play your move! Rock, Paper or Scissors?"), getComputerChoice());
+
+    if (playRoundResult === 0) {
+      tally = tally + 0;
+    }
+    else if (playRoundResult === 1) {
+      tally = tally + 1;
+    }
+    else if (playRoundReturn === -1) {
+      tally = tally + 0;
+    }
+
+    console.log(playRoundReturn);
+  
+  }
+
+  if(tally >=3) {
+    return `You've won the game! You won ${tally} rounds!`;
+  }
+  else {
+    return `You've lost the game. You won only ${tally} rounds!`;
+  }
+}
+
+console.log(game());
